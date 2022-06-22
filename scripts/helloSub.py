f3133540715d080d6bca9c38417f03bce4a023f2
Isak Aasemoen Aardal <595489@stud.hvl.no>
2022-06-22 08:21:24 +0200

    Scripts folder with publisher and subscriber

---
+++ scripts/helloSub.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+#license: MIT
+
+import rospy
+from std_msgs.msg import String
+
+def callback(data):
+    rospy.loginfo(rospy.get_caller_id() + "I just recieved a whisper: %s", data.data)
+
+def listener():
+    rospy.init_node('soundGatherer', anonymous=True)
+    
+    rospy.Subscriber("chatter", String, callback)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
